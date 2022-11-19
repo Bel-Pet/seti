75cd7e9854d01d02f213bf938a2ab6116cfee53e
Bel <belpet@list.ru>
2022-11-19 19:09:24 +0700

    fix dns and add Request

---
+++ lab5/src/main/java/org/example/Request.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+public class Request {
+    private final byte[] buffer;
+
+    public static byte[] connectRequest() {
+        return new byte[]{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    }
+
+    public Request(byte[] buffer) {
+        this.buffer = buffer;
+    }
+
+    public boolean isVersionSocks5() {
+        return buffer[0] == 0x05;
+    }
+
+    public boolean isIPv4() {
+        return buffer[3] == 0x01;
+    }
+
+    public boolean isDomain() {
+        return buffer[3] == 0x03;
+    }
+
+    public boolean isIPv6() {
+        return buffer[3] == 0x04;
+    }
+
+    public boolean isConnectCommand() {
+        return buffer[1] == 0x01;
+    }
+
+    public InetSocketAddress getInetSocketAddress() throws UnknownHostException {
+        InetAddress connectAddr = InetAddress.getByAddress(new byte[]{buffer[4], buffer[5], buffer[6], buffer[7]});
+        int connectPort = ((buffer[8] & 0xFF) << 8) + (buffer[8 + 1] & 0xFF);
+        return new InetSocketAddress(connectAddr, connectPort);
+    }
+
+    public String getHost() {
+        return new String(Arrays.copyOfRange(buffer, 5, 5 +  buffer[4]));
+    }
+
+    public int getPort() {
+        return 5 + buffer[4];
+    }
+
+    private boolean findNoAuthMethod() {
+        for (int i = 0; i < buffer[1]; i++) {
+            if (buffer[i + 2] == 0x00) return true;
+        }
+        return false;
+    }
+
+    public byte[] getRequestWithMethod() {
+        return findNoAuthMethod() ? new byte[]{0x05, 0x00} : new byte[]{0x05, (byte) 0xFF};
+    }
+}
--- lab5/src/main/java/org/example/Socks5Proxy.java
+++ lab5/src/main/java/org/example/Socks5Proxy.java
@@ -1,5 +1,7 @@
 package org.example;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import org.xbill.DNS.Record;
 import org.xbill.DNS.*;
@@ -10,13 +12,11 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.channels.spi.SelectorProvider;
-import java.util.Arrays;
-
-import static java.lang.String.format;
 
 
 public class Socks5Proxy extends Thread {
-    private final int bufferSize = 8192;
+    //private static final Logger LOGGER = LoggerFactory.getLogger(Socks5Proxy.class);
+    private final int BUFF_SIZE = 8192;
     private final int port;
 
     private static class Attachment {
@@ -36,7 +36,7 @@ public class Socks5Proxy extends Thread {
         try (Selector selector = SelectorProvider.provider().openSelector();
              ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
 
-            System.out.println("Server started...");
+            //LOGGER.info("Server started...");
 
             serverChannel.configureBlocking(false);
             serverChannel.socket().bind(new InetSocketAddress(port));
@@ -56,7 +56,7 @@ public class Socks5Proxy extends Thread {
         }
     }
 
-    private void handleKey(SelectionKey key) throws IOException {
+    private void handleKey(SelectionKey key) {
         if (key.isValid()) {
             try {
                 if (key.isAcceptable()) {
@@ -70,7 +70,6 @@ public class Socks5Proxy extends Thread {
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                close(key);
             }
         }
     }
@@ -78,29 +77,37 @@ public class Socks5Proxy extends Thread {
         SocketChannel channel = ((ServerSocketChannel) key.channel()).accept();
         channel.configureBlocking(false);
         channel.register(key.selector(),SelectionKey.OP_READ);
-        System.out.println("Connect from: " + channel.getRemoteAddress());
+        //LOGGER.info("Accept: " + channel.getRemoteAddress());
     }
 
     private void connect(SelectionKey key) throws IOException {
         SocketChannel channel =  (SocketChannel)key.channel();
         Attachment attachment = ((Attachment) key.attachment());
 
-        channel.finishConnect();
+        try {
+            while (channel.isConnectionPending()) {
+                //LOGGER.info("Finish connect: " + channel.getRemoteAddress());
+                channel.finishConnect();
 
-        attachment.in = ByteBuffer.allocate(bufferSize);
-        attachment.in.put(Utils.CONNECTION_OK_REPLY).flip();
-        attachment.out = ((Attachment) attachment.peer.attachment()).in;
-        ((Attachment) attachment.peer.attachment()).out = attachment.in;
-        attachment.peer.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-        key.interestOps(0);
+                attachment.in = ByteBuffer.allocate(BUFF_SIZE);
+                attachment.in.put(Request.connectRequest()).flip();
+                attachment.out = ((Attachment) attachment.peer.attachment()).in;
+                ((Attachment) attachment.peer.attachment()).out = attachment.in;
+                attachment.peer.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+                key.interestOps(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            close(key);
+        }
     }
 
     private void read(SelectionKey key) throws IOException {
 
         var attachment = (Attachment)key.attachment();
         if (attachment == null) {
-            attachment= new Attachment();
-            attachment.in = ByteBuffer.allocate(bufferSize);
+            key.attach(attachment= new Attachment());
+            attachment.in = ByteBuffer.allocate(BUFF_SIZE);
             attachment.type = OperationType.AUTH_READ;
             key.attach(attachment);
         }
@@ -109,13 +116,13 @@ public class Socks5Proxy extends Thread {
             var channel = (DatagramChannel) key.channel();
             if (channel.read(attachment.in) <= 0) {
                 close(key);
-                throw new IOException("Invalid DNS reply");
+                throw new RuntimeException("Invalid DNS reply");
             } else {
                 var message = new Message(attachment.in.array());
                 var maybeRecord = message.getSection(Section.ANSWER).stream().findAny();
                 if (maybeRecord.isPresent()) {
-                    var ipAddr = InetAddress.getByName(maybeRecord.get().rdataToString());
-                    registerPeer(ipAddr, attachment.port, attachment.peer);
+                    registerIPv4(new InetSocketAddress(InetAddress.getByName(maybeRecord.get().rdataToString()),
+                            attachment.port), attachment.peer);
                     key.interestOps(0);
                     key.cancel();
                     key.channel().close();
@@ -129,126 +136,105 @@ public class Socks5Proxy extends Thread {
             SocketChannel channel = (SocketChannel) key.channel();
 
             try {
+                //if (!attachment.peer.isValid()) {close(key);} else
                 if (!channel.isConnected() || channel.read(attachment.in) <= 0) {
                     close(key);
                 } else if (attachment.type == OperationType.AUTH_READ) {
-                    readAndRequestAuthMessage(key);
+                    choiceMethod(key);
                 } else if (attachment.peer == null) {
-                    readAndRequestConnectionMessage(key);
+                    requestConnectionMessage(key);
                 } else {
+                    //LOGGER.info("Read data: " + " " + channel.getRemoteAddress());
                     attachment.peer.interestOps(attachment.peer.interestOps() | SelectionKey.OP_WRITE);
                     key.interestOps(key.interestOps() ^ SelectionKey.OP_READ);
                     attachment.in.flip();
                 }
             } catch (IOException e) {
+                close(key);
                 e.printStackTrace();
             }
         }
     }
 
-    private void readAndRequestAuthMessage(SelectionKey key) throws IllegalStateException{
+    private void choiceMethod(SelectionKey key) throws IOException {
         var attachment = (Attachment)key.attachment();
-        byte[] buffer = attachment.in.array();
-        if (buffer[0] != Utils.VERSION) return;
-
-        int methodsCount = buffer[1];
-        boolean isNoAuthMethodFound = false;
-        for (int i = 0; i < methodsCount; i++) {
-            var method = buffer[i + 2];
-            if (method == Utils.NO_AUTH) {
-                isNoAuthMethodFound = true;
-                break;
-            }
+        Request message = new Request(attachment.in.array());
+        if (!message.isVersionSocks5()) {
+            close(key);
+            throw new RuntimeException("Bad request: incorrect SOCKS version");
         }
-
-        if (!isNoAuthMethodFound)
-            throw new RuntimeException("Auth request has no no auth method, only no auth method is supported");
 
         attachment.out = attachment.in;
         attachment.out.clear();
-        attachment.out.put(Utils.AUTH_NO_AUTH_REPLY).flip();
+        attachment.out.put(message.getRequestWithMethod()).flip();
         attachment.type = OperationType.AUTH_WRITE;
         key.interestOps(SelectionKey.OP_WRITE);
     }
 
-    private void readAndRequestConnectionMessage(SelectionKey key) throws IOException {
+    private void requestConnectionMessage(SelectionKey key) throws IOException {
 
         var attachment = (Attachment) key.attachment();
-        var len = attachment.in.position();
-        if (len < 4) return;
 
-        var buffer = attachment.in.array();
-        if (buffer[0] != Utils.VERSION)
-            throw new RuntimeException("Invalid version, only SOCKS5 is supported");
+        Request buffer = new Request(attachment.in.array());
+        if (!buffer.isVersionSocks5() || !buffer.isConnectCommand() || buffer.isIPv6())
+            throw new RuntimeException("Bad request: unable to process one of the parameters");
 
-        if (buffer[1] != Utils.CONNECTION_COMMAND)
-            throw new RuntimeException("Invalid command, only connect is supported");
+        if (buffer.isIPv4())
+            registerIPv4(buffer.getInetSocketAddress(), key);
 
+        if (buffer.isDomain())
+            registerHost(buffer.getHost(), buffer.getPort(), key);
 
-        if (buffer[3] == Utils.ADDR_TYPE_IPV4) {
-            var connectAddrBytes = new byte[]{buffer[4], buffer[5], buffer[6], buffer[7]};
-            var connectAddr = InetAddress.getByAddress(connectAddrBytes);
-            var portPos = 8;
-            var connectPort = ((buffer[portPos] & 0xFF) << 8) + (buffer[portPos + 1] & 0xFF);
-            registerPeer(connectAddr, connectPort, key);
-            key.interestOps(0);
-        } else if (buffer[3] == Utils.ADDR_TYPE_HOST) {
-            var hostLen = buffer[4];
-            var hostStart = 5;
-            var host = new String(Arrays.copyOfRange(buffer, hostStart, hostStart + hostLen));
-            var portPos = hostStart + hostLen;
-            var connectPort = ((buffer[portPos] & 0xFF) << 8) + (buffer[portPos + 1] & 0xFF);
-            key.interestOps(0);
-            requestHostResolve(host, connectPort, key);
-        }
+        key.interestOps(key.interestOps() & ~SelectionKey.OP_READ);
     }
 
-    private void registerPeer(InetAddress connectAddr, int connectPort, SelectionKey backKey) throws IOException {
+    private void registerIPv4(InetSocketAddress connectAddr, SelectionKey backKey) throws IOException {
         var peer = SocketChannel.open();
         peer.configureBlocking(false);
-        peer.connect(new InetSocketAddress(connectAddr, connectPort));
-        var peerKey = peer.register(backKey.selector(), SelectionKey.OP_CONNECT);
+        peer.connect(connectAddr);
 
+        var peerKey = peer.register(backKey.selector(), SelectionKey.OP_CONNECT);
         ((Attachment) backKey.attachment()).peer = peerKey;
+        ((Attachment) backKey.attachment()).in.clear();
+
         Attachment peerAttachment = new Attachment();
         peerAttachment.peer = backKey;
         peerKey.attach(peerAttachment);
-
-        ((Attachment) backKey.attachment()).in.clear();
     }
 
-    private void requestHostResolve(String host, int backPort, SelectionKey backKey) throws IOException {
-        var peer = DatagramChannel.open();
-        peer.connect(ResolverConfig.getCurrentConfig().server());
-        peer.configureBlocking(false);
+    private void registerHost(String host, int backPort, SelectionKey backKey) throws IOException {
 
-        var key = peer.register(backKey.selector(), SelectionKey.OP_WRITE);
+        var message = new Message();
+        var record = Record.newRecord(Name.fromString(host + '.').canonicalize(), Type.A, DClass.IN);
+        message.addRecord(record, Section.QUESTION);
+        message.getHeader().setOpcode(Opcode.QUERY);
+        message.getHeader().setFlag(Flags.RD);
+        //header.setFlag(Flags.AD);
+        //header.setOpcode(Opcode.QUERY);
+        //header.setFlag(Flags.RD);
 
         Attachment attachment = new Attachment();
         attachment.type = OperationType.DNS_WRITE;
         attachment.port = backPort;
         attachment.peer = backKey;
-        attachment.in = ByteBuffer.allocate(bufferSize);
-
-        var message = new Message();
-        var record = Record.newRecord(Name.fromString(host + '.').canonicalize(), org.xbill.DNS.Type.A, DClass.IN);
-        message.addRecord(record, Section.QUESTION);
-
-        var header = message.getHeader();
-
-        header.setFlag(Flags.AD);
-        header.setFlag(Flags.RD);
-
+        attachment.in = ByteBuffer.allocate(BUFF_SIZE);
         attachment.in.put(message.toWire());
         attachment.in.flip();
         attachment.out = attachment.in;
+
+        var peer = DatagramChannel.open();
+        peer.connect(ResolverConfig.getCurrentConfig().server());
+        peer.configureBlocking(false);
+        var key = peer.register(backKey.selector(), SelectionKey.OP_WRITE);
 
         key.attach(attachment);
 
     }
 
     private void write(SelectionKey key) throws IOException {
+
         Attachment attachment = (Attachment) key.attachment();
+
         if (attachment.type == OperationType.DNS_WRITE) {
             var channel = ((DatagramChannel) key.channel());
 
@@ -257,20 +243,20 @@ public class Socks5Proxy extends Thread {
             } else if (attachment.out.remaining() == 0) {
                 attachment.out.clear();
                 attachment.type = OperationType.DNS_READ;
-                key.interestOpsOr(SelectionKey.OP_READ);
-                key.interestOpsAnd(~SelectionKey.OP_WRITE);
+                key.interestOps(key.interestOps() & ~SelectionKey.OP_WRITE | SelectionKey.OP_READ);
             }
         } else {
-            SocketChannel channel = (SocketChannel) key.channel();
-            ByteBuffer buffer = attachment.out;
-            if (channel.write(buffer) == -1) {
-                close(key);
-            } else if (buffer.remaining() == 0) {
-                if (attachment.type == OperationType.AUTH_WRITE) {
 
+            SocketChannel channel = (SocketChannel) key.channel();
+
+            if (channel.write(attachment.out) == -1) {
+                close(key);
+            } else if (attachment.out.remaining() == 0) {
+                if (attachment.type == OperationType.AUTH_WRITE) {
                     attachment.out.clear();
                     key.interestOps(SelectionKey.OP_READ);
                     attachment.type = OperationType.READ;
+
                 } else if (attachment.peer == null) {
                     close(key);
                 } else {
@@ -289,7 +275,7 @@ public class Socks5Proxy extends Thread {
         SelectionKey peerKey = ((Attachment) key.attachment()).peer;
         if (peerKey != null) {
             ((Attachment)peerKey.attachment()).peer = null;
-            if((peerKey.interestOps()&SelectionKey.OP_WRITE) == 0 ) {
+            if((peerKey.interestOps() & SelectionKey.OP_WRITE) == 0 ) {
                 ((Attachment)peerKey.attachment()).out.flip();
             }
             peerKey.interestOps(SelectionKey.OP_WRITE);
